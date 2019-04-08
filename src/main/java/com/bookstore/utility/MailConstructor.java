700cf99f56eff5268ed68df5b975a5e4555e76da
Gen Gulay <gengulay@gmail.com>
2019-04-09 00:30:12 +0800

    first commit

---
+++ src/main/java/com/bookstore/utility/MailConstructor.java
@@ -0,0 +1,33 @@
+package com.bookstore.utility;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+import com.bookstore.domain.User;
+
+@Component
+public class MailConstructor {
+
+	@Autowired
+	private Environment env;
+
+	public SimpleMailMessage construcResetTokenEmail(String contextPath, Locale locale, String token, User user,
+			String password) {
+		String url = contextPath + "/newUser?token=" + token;
+		String message = "\n Please click this link to verify your email address and edit your personal info your password is: \n"
+				+ password;
+
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setTo(user.getEmail());
+		email.setSubject("Gens Bookstore - new User");
+		email.setText(url + message);
+		email.setFrom(env.getProperty("support.email"));
+		return email;
+
+	}
+
+}
